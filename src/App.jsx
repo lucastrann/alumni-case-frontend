4d3f52d273f9ac47390a869e71c52b3bfdea391c
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-10-02 14:59:24 +0200

    removed something

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -23,7 +23,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the React logo to learn more
       </p>
     </>
   )
