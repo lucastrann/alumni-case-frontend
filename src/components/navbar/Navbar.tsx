e6d685bb4421e268b0c354cdf779b7f4740ca5e6
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-10-10 14:46:41 +0200

    navbar fixes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -16,29 +16,86 @@ const Navbar: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Box as="nav" bg="blue.500" py={2} px={4} boxShadow="md">
+    <Box
+      as="nav"
+      bg={colorMode === 'dark' ? 'gray.800' : 'gray.150'}
+      py={3}
+      px={4}
+      boxShadow="md"
+      borderBottom="1px solid"
+      borderColor={colorMode === 'dark' ? 'gray.600' : 'gray.300'}
+    >
       <Flex alignItems="center">
-        <Heading size="md" color="white">
+        <Heading
+          size="md"
+          color={colorMode === 'dark' ? 'white' : 'gray.800'}
+          fontFamily="monospace"
+          transition="transform 0.3s ease-in-out" // Added CSS transition
+          _hover={{ transform: 'scale(1.03)' }} // Scale up on hover
+        >
           Alumni
         </Heading>
         <Spacer />
         <Flex alignItems="center">
-          <Link as={RouterLink} to="/" color="white" mr={20}>
+          <Link
+            as={RouterLink}
+            to="/"
+            color={colorMode === 'dark' ? 'white' : 'gray.800'}
+            mr={10}
+            _hover={{
+              color: colorMode === 'dark' ? 'teal.300' : 'teal.600',
+              transform: 'scale(1.25)', // Scale up on hover
+            }}
+          >
             Home
           </Link>
-          <Link as={RouterLink} to="/calendar" color="white" mr={20}>
+          <Link
+            as={RouterLink}
+            to="/calendar"
+            color={colorMode === 'dark' ? 'white' : 'gray.800'}
+            mr={10}
+            _hover={{
+              color: colorMode === 'dark' ? 'teal.300' : 'teal.600',
+              transform: 'scale(1.25)', // Scale up on hover
+            }}
+          >
             Calendar
           </Link>
-          <Link as={RouterLink} to="/profile" color="white">
+          <Link
+            as={RouterLink}
+            to="/profile"
+            color={colorMode === 'dark' ? 'white' : 'gray.800'}
+            _hover={{
+              color: colorMode === 'dark' ? 'teal.300' : 'teal.600',
+              transform: 'scale(1.25)', // Scale up on hover
+            }}
+          >
             Profile
           </Link>
         </Flex>
         <Spacer />
+        <Button
+          variant="outline"
+          borderColor={colorMode === 'dark' ? 'teal.300' : 'teal.600'}
+          color={colorMode === 'dark' ? 'white' : 'gray.800'}
+          _hover={{
+            bg: colorMode === 'dark' ? 'teal.300' : 'teal.600',
+            color: 'white',
+          }}
+          onClick={toggleColorMode}
+        >
+          {colorMode === 'dark' ? 'Light Mode' : 'Dark Mode'}
+        </Button>
         <IconButton
           aria-label="Toggle Dark Mode"
           icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+          color={colorMode === 'dark' ? 'white' : 'gray.800'}
+          bg="transparent"
+          _hover={{
+            bg: 'transparent',
+            color: colorMode === 'dark' ? 'teal.300' : 'teal.600',
+          }}
           onClick={toggleColorMode}
-          color="white"
         />
       </Flex>
     </Box>
