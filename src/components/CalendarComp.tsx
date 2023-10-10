56e018523c091bfb1e88c33602d8e71a367109e3
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-10-10 13:34:54 +0200

    added css to modalcalendar

--- src/components/CalendarComp.tsx
+++ src/components/CalendarComp.tsx
@@ -1,52 +1,51 @@
+// CalendarComp.tsx
 import React, { useState } from 'react';
 import { Button, Box, Center, useDisclosure } from '@chakra-ui/react';
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import 'moment/locale/en-gb';
-import ModalComponent from './modal/ModalComponent';
+import ModalCalendar from './modal/ModalCalendar';
 
 moment.locale('en-gb');
 
 const localizer = momentLocalizer(moment);
 
 const CalendarComp = () => {
-  // Define the type for events
   const [events, setEvents] = useState<Event[]>([]);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // Define the type for the Event object
   type Event = {
     title: string;
     start: Date;
     end: Date;
   };
 
-  // Handle adding a new event
   const handleNewEvent = (newEvent: Event) => {
     setEvents([...events, newEvent]);
-    onClose(); // Close the modal after adding the new event
+    onClose();
   };
 
-  const handleConfirm = (eventTitle: string) => {
-    const dateString = '15/10/2023'; // Assuming DD/MM/YYYY format
+  const handleConfirm = (eventTitle: string, startDate: string, endDate: string) => {
+    const partsStart = startDate.split('/');
+    const dayStart = parseInt(partsStart[0], 10);
+    const monthStart = parseInt(partsStart[1], 10) - 1; 
+    const yearStart = parseInt(partsStart[2], 10);
 
+    const partsEnd = endDate.split('/');
+    const dayEnd = parseInt(partsEnd[0], 10);
+    const monthEnd = parseInt(partsEnd[1], 10) - 1;
+    const yearEnd = parseInt(partsEnd[2], 10);
 
-    const parts = dateString.split('/');
-    const day = parseInt(parts[0], 10);
-    const month = parseInt(parts[1], 10) - 1; // Months in JavaScript are zero-based (0-11)
-    const year = parseInt(parts[2], 10);
-    
-
-    const start = new Date(year, month, day, 0, 0, 0); 
-    const end = new Date(year, month, day, 23, 59, 59);
+    const start = new Date(yearStart, monthStart, dayStart, 0, 0, 0); 
+    const end = new Date(yearEnd, monthEnd, dayEnd, 23, 59, 59);
 
     const newEvent: Event = {
       title: eventTitle,
-      start: new Date(start), // You can set the start date as needed
-      end: new Date(end),   // You can set the end date as needed
+      start: new Date(start), 
+      end: new Date(end),  
     };
-    console.log(newEvent)
+
     handleNewEvent(newEvent);
   };
 
@@ -59,14 +58,14 @@ const CalendarComp = () => {
         <BigCalendar
           localizer={localizer}
           events={events}
-          startAccessor={(event) => event.start} // Use accessor functions
-          endAccessor={(event) => event.end} // Use accessor functions
+          startAccessor={(event) => event.start}
+          endAccessor={(event) => event.end}
           style={{ height: 500 }}
         />
-        <ModalComponent
+        <ModalCalendar
           isOpen={isOpen}
           onClose={onClose}
-          onConfirm={handleConfirm} // Pass the handleConfirm function
+          onConfirm={handleConfirm}
           title="Create New Event"
           placeholder="Event Title"
         />
---
+++ src/components/modal/ModalCalendar.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import DatePicker from "react-datepicker"; 
+import "react-datepicker/dist/react-datepicker.css";
+
+
+interface ModalCalendarProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: (title: string, startDate: string, endDate: string) => void;
+  title: string;
+  placeholder: string;
+}
+
+const ModalCalendar: React.FC<ModalCalendarProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  title,
+  placeholder,
+}) => {
+  const [eventTitle, setEventTitle] = useState("");
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+
+  const handleConfirmClick = () => {
+    if (eventTitle && startDate && endDate) {
+      const formattedStartDate = startDate.toLocaleDateString();
+      const formattedEndDate = endDate.toLocaleDateString(); 
+
+      onConfirm(eventTitle, formattedStartDate, formattedEndDate); 
+      setEventTitle(""); 
+      setStartDate(null);
+      setEndDate(null);
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Input
+            placeholder="Event Title"
+            value={eventTitle}
+            onChange={(e) => setEventTitle(e.target.value)}
+            mb="2"
+          />
+          <div className="date-picker-container">
+            <DatePicker
+              selected={startDate}
+              onChange={(date: Date | null) => setStartDate(date)}
+              dateFormat="dd/MM/yyyy"
+              placeholderText="Start Date"
+              className="date-picker"
+            />
+            <DatePicker
+              selected={endDate}
+              onChange={(date: Date | null) => setEndDate(date)}
+              dateFormat="dd/MM/yyyy"
+              placeholderText="End Date"
+              className="date-picker"
+            />
+          </div>
+          <Flex justifyContent="flex-end">
+            <Button colorScheme="teal" onClick={handleConfirmClick}>
+              Confirm
+            </Button>
+          </Flex>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalCalendar;
