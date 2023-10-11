7a0a1ddd7a446e3a43ae23e02775d4bc5f31617d
lucastrann <71780014+lucastrann@users.noreply.github.com>
2023-10-11 10:59:21 +0200

    added comment and updated the userdata with comment

--- src/components/home/Post.tsx
+++ src/components/home/Post.tsx
@@ -1,35 +1,51 @@
 import React, { useState } from 'react';
 import { Box, Text, Avatar, Button, Input } from '@chakra-ui/react';
-import { Post, userData } from './data';
+import { Post, userData, Comment, UserData } from './data';
 import '../css/Feed.css';
 
 interface PostProps {
   post: Post;
 }
 
-const userName = userData.user.name
-
 const PostComponent: React.FC<PostProps> = ({ post }) => {
   const [newComment, setNewComment] = useState('');
 
+  // Use a state variable to manage the user data
+  const [updatedUserData, setUpdatedUserData] = useState<UserData>(userData);
+
   const handleAddComment = () => {
     if (newComment.trim() !== '') {
-      // Add a new comment to the post's comments array
-      post.comments.push({
-        id: post.comments.length + 1,
-        author: userData.user.name,
-        text: newComment,
-        timestamp: new Date().toUTCString(),
-      });
-      setNewComment('');
+      const updatedPost = updatedUserData.posts.find((p) => p.id === post.id);
+
+      if (updatedPost) {
+        const newCommentData: Comment = {
+          id: updatedPost.comments.length + 1, // Generate a new comment ID
+          author: updatedUserData.user.name,
+          text: newComment,
+          timestamp: new Date().toUTCString(),
+        };
+
+        updatedPost.comments.push(newCommentData);
+
+        // Update the user data with the modified post
+        setUpdatedUserData({
+          ...updatedUserData,
+          posts: updatedUserData.posts.map((p) =>
+            p.id === post.id ? updatedPost : p
+          ),
+        });
+
+        setNewComment('');
+      }
+      console.log(updatedUserData);
     }
   };
 
   return (
     <Box className="post">
       <Box className="post-header">
-        <Avatar name={userName} src={userData.user.avatar} />
-        <Text className="author">{userName}</Text>
+        <Avatar name={updatedUserData.user.name} src={updatedUserData.user.avatar} />
+        <Text className="author">{updatedUserData.user.name}</Text>
       </Box>
       <Text className="post-text">{post.text}</Text>
 
